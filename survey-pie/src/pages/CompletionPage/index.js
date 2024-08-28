79f65e48f3e0109af86f34328b9e8835d561f0aa
kdhn97 <ehdgus3726@naver.com>
2024-08-28 10:54:08 +0900

    0828

--- survey-pie/src/pages/CompletionPage/index.js
+++ survey-pie/src/pages/CompletionPage/index.js
@@ -1,10 +1,13 @@
+import axios from "axios";
 import styled from "styled-components";
 import { useRecoilValue } from "recoil";
-// import testState from "../../stores/test/atom";
 import testWithComma from "../../stores/test/testWithComma";
 
 function CompletionPage() {
   const test = useRecoilValue(testWithComma);
+  axios.get("http://localhost:3001/surveys").then((res) => {
+    console.log("res", res.data);
+  });
   return <CompletionPageWrapper>{test}</CompletionPageWrapper>;
 }
 const CompletionPageWrapper = styled.div``;
